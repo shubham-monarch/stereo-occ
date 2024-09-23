e80f19c2d78b73ebad7b15c9e6f8c50f7f213039
Shubham Kumar <skumar@monarchtractor.com>
2024-09-23 22:43:02 +0530

    fixed cam-view-control

--- bev-voxelizer/main.py
+++ bev-voxelizer/main.py
@@ -35,27 +35,27 @@ def get_random_segmented_pcd(src_foler: Path) -> Path:
 #     return False
 
 
-def update_camera_view(vis, point_cloud):
-    # Create coordinate frame
+# def update_camera_view(vis, point_cloud):
+#     # Create coordinate frame
     
-    # Set the camera view to be at the pointcloud origin
-    ctr = vis.get_view_control()
+#     # Set the camera view to be at the pointcloud origin
+#     ctr = vis.get_view_control()
 
-    bbox = point_cloud.to_legacy().get_axis_aligned_bounding_box()
-    bbox_min = bbox.get_min_bound()
-    bbox_max = bbox.get_max_bound()
-    dimensions = bbox_max - bbox_min
+#     bbox = point_cloud.to_legacy().get_axis_aligned_bounding_box()
+#     bbox_min = bbox.get_min_bound()
+#     bbox_max = bbox.get_max_bound()
+#     dimensions = bbox_max - bbox_min
 
-    center = bbox.get_center()
-    ctr.set_lookat(center)
+#     center = bbox.get_center()
+#     # ctr.set_lookat(center)
     
-    # Set the camera position 
-    ctr.set_front([0, 0, -1])  # Looking along negative z-axis
-    # ctr.set_up([0, 1, 0])     # Up direction is negative y-axis (Open3D convention)
-    
-    ctr.set_zoom(0.8)
+#     # Set the camera position 
+#     # ctr.set_front([0, 0, -1])  # Looking along negative z-axis
+#     # ctr.set_up([0, 1, 0])     # Up direction is negative y-axis (Open3D convention)
 
-    return vis
+#     # ctr.set_zoom(0.8)
+
+#     return vis
 
 
 def calculate_angles(normal_vector):
@@ -96,7 +96,7 @@ if __name__ == "__main__":
     vis.add_geometry(coordinate_frame)
 
     # adjusting default camera view for better visualization
-    vis = update_camera_view(vis, pcd)
+    # vis = update_camera_view(vis, pcd)
     
     
     
@@ -157,8 +157,13 @@ if __name__ == "__main__":
     # # vis.add_geometry(pcd.to_legacy())
     # vis.add_geometry(pcd_ground_transformed.to_legacy())
 
-
-
+    # update camera view
+    view_ctr = vis.get_view_control()
+    
+    # view_ctr.rotate(180, 0)
+    view_ctr.set_front(np.array([0, 0, -1]))
+    view_ctr.set_up(np.array([0, -1, 0]))
+    
     vis.run()
     vis.destroy_window()
 
