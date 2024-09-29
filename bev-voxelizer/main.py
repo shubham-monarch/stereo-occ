2fc741826c84034c021840617ac3f0a8793ec6e9
Shubham Kumar <skumar@monarchtractor.com>
2024-09-30 01:35:32 +0530

    bev automation added

--- bev-voxelizer/main.py
+++ bev-voxelizer/main.py
@@ -10,6 +10,7 @@ import random
 import numpy as np  
 from collections import defaultdict
 import matplotlib.pyplot as plt
+import time
 
 # custom modules
 from utils import io_utils
@@ -33,7 +34,6 @@ logger.addHandler(handler)
 coloredlogs.install(level='INFO', logger=logger, force=True)
 
 
-
 class BevVoxelizer:
     def __init__(self):
         
@@ -44,7 +44,6 @@ class BevVoxelizer:
             "VINE_STEM": {"id": 4, "priority": 4},  
             "NAVIGABLE_SPACE": {"id": 2, "priority": 5},  
         }
-
     
     def filter_radius_outliers(self, pcd, nb_points, search_radius):
         '''
@@ -283,9 +282,9 @@ class BevVoxelizer:
         logger.info(f"=================================\n")
         
         # radius-based outlier removal
-        rad_filt_pole, _ = self.filter_radius_outliers(down_pole, nb_points=16, search_radius=0.05)
-        rad_filt_stem, _ = self.filter_radius_outliers(down_stem, nb_points=16, search_radius=0.05)
-        rad_filt_obstacle, _ = self.filter_radius_outliers(down_obstacle, nb_points=16, search_radius=0.05)
+        rad_filt_pole = down_pole if len(down_pole.point['positions']) == 0 else self.filter_radius_outliers(down_pole, nb_points=16, search_radius=0.05)[0]
+        rad_filt_stem = down_stem if len(down_stem.point['positions']) == 0 else self.filter_radius_outliers(down_stem, nb_points=16, search_radius=0.05)[0]
+        rad_filt_obstacle = down_obstacle if len(down_obstacle.point['positions']) == 0 else self.filter_radius_outliers(down_obstacle, nb_points=16, search_radius=0.05)[0]
         # rad_filt_navigable, _ = filter_radius_outliers(down_navigable, nb_points=16, search_radius=0.05)
         # rad_filt_canopy, _ = filter_radius_outliers(down_canopy, nb_points=1, search_radius=0.1)
         
@@ -316,7 +315,7 @@ class BevVoxelizer:
         normal = normal / np.linalg.norm(normal)
         inliers_navigable = pcd_navigable.select_by_index(inliers)
 
-        self.plot_plane_histogram(inliers_navigable)
+        # self.plot_plane_histogram(inliers_navigable)
         
         # compute angle with y-axis
         angle_y = self.axis_angles(normal)[1]
@@ -348,50 +347,98 @@ class BevVoxelizer:
         bev_canopy = down_canopy.clone()
         bev_canopy.point['positions'][:, 1] = float(mean_y_value) - 0.07
 
-
         bev_collection = [inliers_navigable, bev_canopy, bev_pole, bev_stem, bev_obstacle]
-
         combined_pcd = self.generate_unified_bev_pcd(bev_collection)
-        
         return combined_pcd
 
+def bev_images_from_foler(src_folder: str, bev_images_folder: str):
+    '''
+    Generate BEV images for all pointclouds in the source folder
+    '''
+    os.makedirs(bev_images_folder, exist_ok=True)          
+    vis = o3d.visualization.Visualizer()
+  
+    for file_name in tqdm(np.random.permutation(os.listdir(src_folder)), desc="Processing files"):
+        if "_treasury" in file_name and file_name.endswith(".ply"):
+            try:
+                logger.warning(f"=================================")        
+                logger.warning(f"Processing {file_name}")
+                logger.warning(f"=================================\n")
+                
+                src_path = os.path.join(src_folder, file_name)
+                pcd_input = o3d.t.io.read_point_cloud(src_path)
+                
+                bev_voxelizer = BevVoxelizer()
+                combined_pcd = bev_voxelizer.generate_bev_voxels(pcd_input)
+                
+                avg_x = combined_pcd.point['positions'][:, 0].mean().item()
+                avg_y = combined_pcd.point['positions'][:, 1].mean().item()
+                avg_z = combined_pcd.point['positions'][:, 2].mean().item()
+                logger.info(f"Average x: {avg_x}, Average y: {avg_y}, Average z: {avg_z}")
 
-
+                combined_pcd.point['positions'][:, 2] += 350
+                # combined_pcd.point['positions'][:, 0] -= 500
+                # Save the combined point cloud
+                # combined_pcd_path = os.path.join(bev_pcd_folder, file_name)
+                # o3d.t.io.write_point_cloud(combined_pcd_path, combined_pcd)
+                
+                vis.create_window()
+                
+                # Co-ordinate frame for vis window    
+                coordinate_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1, origin=[0, 0, 0])
+                vis.add_geometry(coordinate_frame)
+                
+                # Adding point clouds to visualizer
+                vis.add_geometry(combined_pcd.to_legacy())
+                
+                view_ctr = vis.get_view_control()
+                view_ctr.set_front(np.array([0, -1, 0]))
+                view_ctr.set_up(np.array([0, 0, 1]))
+                view_ctr.set_zoom(0.1)
+                
+                # Capture the screen and save it as an image
+                vis.poll_events()
+                vis.update_renderer()
+                image_path = os.path.join(bev_images_folder, file_name.replace(".ply", ".png"))
+                vis.capture_screen_image(image_path)
+                time.sleep(3)  # Wait for 1 second
+                vis.destroy_window()
+            except Exception as e:
+                logger.error(f"Error processing {file_name}: {e}")
+            finally:
+                vis.destroy_window()
+            # break
 
 
 if __name__ == "__main__":
     
     src_folder = "pcd-files/vineyards/"
-    src_path = os.path.join(src_folder, "vineyards_RJM_1.ply")
-    pcd_input = o3d.t.io.read_point_cloud(src_path)
+    # bev_images_folder = "bev-images-test"
+    file_name = "vineyards_wente_test_269.ply"
     
+    # os.makedirs(bev_images_folder, exist_ok=True)          
+    vis = o3d.visualization.Visualizer()
+  
+    src_path = os.path.join(src_folder, file_name)
+    pcd_input = o3d.t.io.read_point_cloud(src_path)
     
     bev_voxelizer = BevVoxelizer()
     combined_pcd = bev_voxelizer.generate_bev_voxels(pcd_input)
     
-    # visualization windows
-    vis = o3d.visualization.Visualizer()
+    
     vis.create_window()
-
-    # co-ordinate frame for vis window    
+    
+    # Co-ordinate frame for vis window    
     coordinate_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1, origin=[0, 0, 0])
     vis.add_geometry(coordinate_frame)
-
-    # adding point clouds to visusalizer
+    
+    # Adding point clouds to visualizer
     vis.add_geometry(combined_pcd.to_legacy())
     
     view_ctr = vis.get_view_control()
     view_ctr.set_front(np.array([0, -1, 0]))
     view_ctr.set_up(np.array([0, 0, 1]))
-
-    view_ctr.set_zoom(0.6)
-
-    # Capture the screen and save it as an image
-    vis.poll_events()
-    vis.update_renderer()
-    vis.capture_screen_image("projection_image.png")
-
+    view_ctr.set_zoom(0.5)
+    
     vis.run()
     vis.destroy_window()
-
-    
