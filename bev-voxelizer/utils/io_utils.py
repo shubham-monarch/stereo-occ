0355051835834fac252ce2e53de24c0bbc0d2ca6
Shubham Kumar <skumar@monarchtractor.com>
2024-11-28 14:28:44 +0530

    clean-up

--- bev-voxelizer/utils/io_utils.py
+++ bev-voxelizer/utils/io_utils.py
@@ -78,3 +78,64 @@ def create_folders(folders):
 	for path in folders:
 		os.makedirs(path, exist_ok=True)
 		# logging.warning(f"Created the {path} folder!")
+            
+
+
+
+# def bev_images_from_folder(src_folder: str, bev_images_folder: str):
+#     '''
+#     Generate BEV images for all pointclouds in the source folder
+#     '''
+#     os.makedirs(bev_images_folder, exist_ok=True)          
+#     vis = o3d.visualization.Visualizer()
+  
+#     for file_name in tqdm(np.random.permutation(os.listdir(src_folder)), desc="Processing files"):
+#         if "_treasury" in file_name and file_name.endswith(".ply"):
+#             try:
+#                 logger.warning(f"=================================")        
+#                 logger.warning(f"Processing {file_name}")
+#                 logger.warning(f"=================================\n")
+                
+#                 src_path = os.path.join(src_folder, file_name)
+#                 pcd_input = o3d.t.io.read_point_cloud(src_path)
+                
+#                 bev_voxelizer = BevVoxelizer()
+#                 combined_pcd = bev_voxelizer.generate_bev_voxels(pcd_input)
+                
+#                 avg_x = combined_pcd.point['positions'][:, 0].mean().item()
+#                 avg_y = combined_pcd.point['positions'][:, 1].mean().item()
+#                 avg_z = combined_pcd.point['positions'][:, 2].mean().item()
+#                 logger.info(f"Average x: {avg_x}, Average y: {avg_y}, Average z: {avg_z}")
+
+#                 combined_pcd.point['positions'][:, 2] += 350
+#                 # combined_pcd.point['positions'][:, 0] -= 500
+#                 # Save the combined point cloud
+#                 # combined_pcd_path = os.path.join(bev_pcd_folder, file_name)
+#                 # o3d.t.io.write_point_cloud(combined_pcd_path, combined_pcd)
+                
+#                 vis.create_window()
+                
+#                 # Co-ordinate frame for vis window    
+#                 coordinate_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1, origin=[0, 0, 0])
+#                 vis.add_geometry(coordinate_frame)
+                
+#                 # Adding point clouds to visualizer
+#                 vis.add_geometry(combined_pcd.to_legacy())
+                
+#                 view_ctr = vis.get_view_control()
+#                 view_ctr.set_front(np.array([0, -1, 0]))
+#                 view_ctr.set_up(np.array([0, 0, 1]))
+#                 view_ctr.set_zoom(0.1)
+                
+#                 # Capture the screen and save it as an image
+#                 vis.poll_events()
+#                 vis.update_renderer()
+#                 image_path = os.path.join(bev_images_folder, file_name.replace(".ply", ".png"))
+#                 vis.capture_screen_image(image_path)
+#                 time.sleep(3)  # Wait for 1 second
+#                 vis.destroy_window()
+#             except Exception as e:
+#                 logger.error(f"Error processing {file_name}: {e}")
+#             finally:
+#                 vis.destroy_window()
+#             # break
