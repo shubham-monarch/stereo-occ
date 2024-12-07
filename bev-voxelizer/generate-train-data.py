390964014f65c907ab3d4fd8f6868598ebf87c4f
Shubham Kumar <skumar@monarchtractor.com>
2024-12-08 03:52:57 +0530

    clean-up

--- bev-voxelizer/generate-train-data.py
+++ bev-voxelizer/generate-train-data.py
@@ -1,23 +1,52 @@
-import os
-import cv2
-import numpy as np
-import open3d as o3d
-import json
-from tqdm import tqdm
+# #! /usr/bin/env python3
 
-from logger import get_logger
+# import open3d as o3d
+# import os
+# import random
+# import numpy as np
+# import cv2
+# import shutil
+# from tqdm import tqdm
+# import yaml
 
-logger = get_logger("generate-train-data")
+# from bev_voxelizer import BevVoxelizer
+# from helpers import get_logger
 
-class SBEVDataset:
-    def __init__(self):
-        pass
+
+
+
+# if __name__ == "__main__":
+#     logger = get_logger()
+
+#     segmented_pcd_dir = "/home/skumar/ssd/2024_06_06_utc"
+#     segmented_bev_dir = "train-data"
+    
+#     segmented_pcd_folders = list_base_folders(segmented_pcd_dir)
     
 
-if __name__ == "__main__":
-    
-    # generate training data for sbevnet -> 
-    # [left.jpg, right.jpg, seg-mask-mono.png, seg-mask-rgb.png, camera-extrinsics.json]
-    # transform to sbevnet co-ordinate frame
-    
-    pass
+#     for i in tqdm(range(200), desc="Processing folders"):
+#         try:
+#             random_folder = random.choice(segmented_pcd_folders)
+            
+#             with open("logs/processing_log.txt", "a") as log_file:
+#                 log_file.write("=================================\n")
+#                 log_file.write(f"Processing {random_folder}\n")
+#                 log_file.write("=================================\n")
+            
+#             random_pcd_file = os.path.join(random_folder, "left-segmented-labelled.ply")
+
+#             bev_image = pcd_to_segmentation_mask_mono(random_pcd_file)
+            
+#             output_path = os.path.join(random_folder, "bev_image_mono.png")
+#             cv2.imwrite(output_path, bev_image)
+
+#             destination_folder = os.path.join(segmented_bev_dir, str(i))
+#             shutil.copytree(random_folder, destination_folder)
+
+#             with open("logs/processing_log.txt", "a") as log_file:
+#                 log_file.write(f"Saved BEV image to {output_path}\n")
+#                 log_file.write(f"Copied {random_folder} to {destination_folder}\n")
+#                 log_file.write("=================================\n")
+
+#         except Exception as e:
+#             logger.error(f"Error processing folder {random_folder}: {e}")
