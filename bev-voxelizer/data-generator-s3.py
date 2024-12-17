225a72b4dd2ac409c27f5824e96c84809459eeb1
Shubham Kumar <skumar@monarchtractor.com>
2024-12-18 04:12:50 +0530

    added get_leaf_folders

--- bev-voxelizer/data-generator-s3.py
+++ bev-voxelizer/data-generator-s3.py
@@ -15,28 +15,68 @@ class LeafFolder:
 class DataGeneratorS3:
     def __init__(self, src_uris: List[str] = None, dest_folder: str = None, index_json: str = None):
         
-        # # Validate that all s3 URIs have occ-dataset as root directory
-        # for uri in src_uris:
-        #     bucket_name, prefix = uri.replace("s3://", "").split("/", 1)
-        #     root_dir = prefix.split("/")[0]
-        #     assert root_dir == "occ-dataset", f"Root directory must be 'occ-dataset', got '{root_dir}' in {uri}"
-        
-        
-        # self.logger = get_logger("data-generator-s3")
-        # self.leaf_folders = self.get_leaf_folders(src_uris)
-        # self.index_json = index_json
         pass
     
     
     
-    def get_target_uri(self, src_uri: str, dest_folder:str = "bev-dataset"):
-        ''' make leaf-folder path relative to the bev-dataset folder '''
+    def get_target_folder_uri(self, src_uri: str, dest_folder:str = "bev-dataset"):
+        ''' Make leaf-folder path relative to the bev-dataset folder '''
         return src_uri.replace("occ-dataset", dest_folder, 1)
         
-
-
+   
 
     
+    def get_leaf_folders(self, s3_uri: str) -> list:
+        """Get all leaf folders from the given S3 URI"""
+        
+        # Parse S3 URI to get bucket and prefix
+        s3_parts = s3_uri.replace("s3://", "").split("/", 1)
+        bucket_name = s3_parts[0]
+        prefix = s3_parts[1] if len(s3_parts) > 1 else ""
+        
+        # Initialize S3 client
+        s3 = boto3.client('s3')
+        
+        # Get all objects with the given prefix
+        paginator = s3.get_paginator('list_objects_v2')
+        pages = paginator.paginate(Bucket=bucket_name, Prefix=prefix)
+        
+        # Keep track of all folders and their parent folders
+        all_folders = set()
+        parent_folders = set()
+        
+        # Process all objects
+        for page in pages:
+            if 'Contents' not in page:
+                continue
+                
+            for obj in page['Contents']:
+                # Get the full path
+                path = obj['Key']
+                
+                # Skip the prefix itself
+                if path == prefix:
+                    continue
+                    
+                # Get all folder paths in this object's path
+                parts = path.split('/')
+                for i in range(len(parts)-1):
+                    folder = '/'.join(parts[:i+1])
+                    if folder:
+                        all_folders.add(folder)
+                        
+                        # If this isn't the immediate parent, it's a parent folder
+                        if i < len(parts)-2:
+                            parent_folders.add(folder)
+        
+        # Leaf folders are those that aren't parents of other folders
+        leaf_folders = all_folders - parent_folders
+        
+        # Convert back to S3 URIs
+        leaf_folder_uris = [f"s3://{bucket_name}/{folder}" for folder in sorted(leaf_folders)]
+        
+        return leaf_folder_uris
+
 if __name__ == "__main__":
     # uri_list = [
     #     "s3://occupancy-dataset/occ-dataset/vineyards/gallo/",
@@ -45,14 +85,19 @@ if __name__ == "__main__":
     logger = get_logger("__main__")
     data_generator_s3 = DataGeneratorS3()
 
-    src_uri = "s3://occupancy-dataset/occ-dataset/vineyards/gallo/2024_06_07_utc/svo_files/front_2024-06-04-10-24-57.svo/1398_to_1540/frame-1400/"
-    target_uri = data_generator_s3.get_target_uri(src_uri, "bev-dataset")
+    # src_uri = "s3://occupancy-dataset/occ-dataset/vineyards/gallo/2024_06_07_utc/svo_files/front_2024-06-04-10-24-57.svo/1398_to_1540/frame-1400/"
+    src_uri = "s3://occupancy-dataset/occ-dataset/"
+    # target_uri = data_generator_s3.get_target_uri(src_uri, "bev-dataset")
 
+    # logger.info(f"===================")
+    # logger.info(f"target_uri: {target_uri}")
+    # logger.info(f"===================\n")
+
+    leaf_folders = data_generator_s3.get_leaf_folders(src_uri)
     logger.info(f"===================")
-    logger.info(f"target_uri: {target_uri}")
+    logger.info(f"len(leaf_folders): {len(leaf_folders)}")
+    logger.info(f"leaf_folders[:20]: {leaf_folders[:20]}")
     logger.info(f"===================\n")
-
-
 
     # leaf_folders = data_generator_s3.get_leaf_folders("s3://occupancy-dataset/occ-dataset/vineyards/gallo/")
     
