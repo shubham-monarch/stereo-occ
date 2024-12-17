d6290d94344770ed9057980d1515c1c6712fbc78
Shubham Kumar <skumar@monarchtractor.com>
2024-12-18 04:20:03 +0530

    added DataGeneratorS3

--- bev-voxelizer/data-generator-s3.py
+++ bev-voxelizer/data-generator-s3.py
@@ -13,21 +13,16 @@ class LeafFolder:
 
 
 class DataGeneratorS3:
-    def __init__(self, src_uris: List[str] = None, dest_folder: str = None, index_json: str = None):
-        
-        pass
-    
-    
-    
+    def __init__(self, src_uris: List[str] = None, dest_folder: str = None, index_json: str = None):    
+        self.logger = get_logger("DataGeneratorS3")
+     
     def get_target_folder_uri(self, src_uri: str, dest_folder:str = "bev-dataset"):
         ''' Make leaf-folder path relative to the bev-dataset folder '''
         return src_uri.replace("occ-dataset", dest_folder, 1)
         
    
-
-    
-    def get_leaf_folders(self, s3_uri: str) -> list:
-        """Get all leaf folders from the given S3 URI"""
+    def get_leaf_folders(self, s3_uri: str) -> List[str]:
+        """Get all leaf folders URI inside the given S3 URI"""
         
         # Parse S3 URI to get bucket and prefix
         s3_parts = s3_uri.replace("s3://", "").split("/", 1)
@@ -77,39 +72,32 @@ class DataGeneratorS3:
         
         return leaf_folder_uris
 
+
+    def generate_bev_dataset(self, src_uri: str, dest_folder: str = "bev-dataset"):
+        ''' Generate a BEV dataset from the given S3 URI '''
+        leaf_URIs = self.get_leaf_folders(src_uri)
+        random.shuffle(leaf_URIs)
+        for idx, leaf_URI in enumerate(leaf_URIs):
+            target_folder = self.get_target_folder_uri(leaf_URI, dest_folder)
+            leaf_folder = LeafFolder(leaf_URI, target_folder)
+
+            self.logger.info(f"===================")
+            self.logger.info(f"leaf_URI: {leaf_URI}")
+            self.logger.info(f"target_folder: {target_folder}")
+            self.logger.info(f"===================")
+
+            if idx > 10:
+                break
+
+            # self.process_leaf_folder(leaf_folder)
+
 if __name__ == "__main__":
     # uri_list = [
     #     "s3://occupancy-dataset/occ-dataset/vineyards/gallo/",
     # ]
 
     logger = get_logger("__main__")
+    
     data_generator_s3 = DataGeneratorS3()
-
-    # src_uri = "s3://occupancy-dataset/occ-dataset/vineyards/gallo/2024_06_07_utc/svo_files/front_2024-06-04-10-24-57.svo/1398_to_1540/frame-1400/"
-    src_uri = "s3://occupancy-dataset/occ-dataset/"
-    # target_uri = data_generator_s3.get_target_uri(src_uri, "bev-dataset")
-
-    # logger.info(f"===================")
-    # logger.info(f"target_uri: {target_uri}")
-    # logger.info(f"===================\n")
-
-    leaf_folders = data_generator_s3.get_leaf_folders(src_uri)
-    logger.info(f"===================")
-    logger.info(f"len(leaf_folders): {len(leaf_folders)}")
-    logger.info(f"leaf_folders[:20]: {leaf_folders[:20]}")
-    logger.info(f"===================\n")
-
-    # leaf_folders = data_generator_s3.get_leaf_folders("s3://occupancy-dataset/occ-dataset/vineyards/gallo/")
-    
-    # logger = get_logger("data-generator-s3")
-    
-    # logger.info(f"===================")
-    # logger.info(f"len(leaf_folders): {len(leaf_folders)}")
-    # logger.info(f"===================")
-
-    
-    # random_values = random.sample(leaf_folders, min(20, len(leaf_folders)))
-    
-    # logger.info(f"===================")
-    # logger.info(f"Random values from leaf folders: {random_values}")
-    # logger.info(f"===================\n")
+    src_uri = "s3://occupancy-dataset/occ-dataset/vineyards/RJM/"
+    data_generator_s3.generate_bev_dataset(src_uri)
