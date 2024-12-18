bb968e66a957e005fbb04bd3859fda4b91ed5b7d
Shubham Kumar <skumar@monarchtractor.com>
2024-12-18 05:40:05 +0530

    added download_segmented_pcd()

--- bev-voxelizer/data-generator-s3.py
+++ bev-voxelizer/data-generator-s3.py
@@ -5,12 +5,81 @@ import os
 import random
 from logger import get_logger
 from typing import List
+from tqdm import tqdm
+
+from bev_generator import BEVGenerator
+
 
 class LeafFolder:
-    def __init__(self, src_uri: str, target_uri: str):
-        self.src_uri = src_uri
-        self.target_uri = target_uri
+    def __init__(self, src_URI: str, dest_URI: str):
+        '''
+        :param src_URI: occ-dataset S3 URI
+        :param dest_URI: bev-dataset S3 URI
+        '''
+        self.logger = get_logger("LeafFolder")
+        self.src_URI = src_URI
+        self.dest_URI = dest_URI
+        self.s3 = boto3.client('s3')
 
+        
+        
+        self.bev_generator = BEVGenerator()
+    
+    def process_folder(self):
+        
+        # 1. download left-segmented-labelled.ply
+        self.download_segmented_pcd()
+
+        # 2. copy imgL
+        # 3. copy imgR
+        # 4. upload seg_mono
+        # 5. upload seg_rgb
+        # 6. upload ipm_fea
+        # 6. upload ipm_rgb
+
+
+
+        pass
+
+    
+
+    def copy_imgL(self, imgL_uri: str):
+        pass
+
+    def copy_imgR(self, imgR_uri: str):
+        pass
+
+    def upload_seg_mono(self, seg_mono_uri: str):
+        pass
+
+    def upload_seg_rgb(self, seg_rgb_uri: str):
+        pass
+
+    def upload_ipm_fea(self, ipm_fea_uri: str):
+        pass
+
+    def upload_ipm_rgb(self, ipm_seg_uri: str):
+        pass
+
+    def download_segmented_pcd(self):
+        self.logger.info(f"=======================")
+        self.logger.info(f"Downloading left-segmented-labelled.ply!")
+        self.logger.info(f"=======================\n")
+        
+        segmented_pcd_uri = self.src_URI + f"left-segmented-labelled.ply" 
+        bucket_name, key = segmented_pcd_uri.replace("s3://", "").split("/", 1)
+                
+        os.makedirs("tmp", exist_ok=True)
+        path_tmp = os.path.join("tmp", "left-segmented-labelled.ply")
+        
+        self.s3.download_file(bucket_name, key, path_tmp)
+        return path_tmp
+
+        
+
+    def generate_bev(self, imgL_uri: str, imgR_uri: str):
+        pass
+    
 
 class DataGeneratorS3:
     def __init__(self, src_uris: List[str] = None, dest_folder: str = None, index_json: str = None):    
@@ -75,21 +144,15 @@ class DataGeneratorS3:
 
     def generate_bev_dataset(self, src_uri: str, dest_folder: str = "bev-dataset"):
         ''' Generate a BEV dataset from the given S3 URI '''
+        
         leaf_URIs = self.get_leaf_folders(src_uri)
         random.shuffle(leaf_URIs)
-        for idx, leaf_URI in enumerate(leaf_URIs):
+        
+        for idx, leaf_URI in tqdm(enumerate(leaf_URIs), total=len(leaf_URIs), desc="Processing leaf URIs"):    
             target_folder = self.get_target_folder_uri(leaf_URI, dest_folder)
             leaf_folder = LeafFolder(leaf_URI, target_folder)
+            leaf_folder.process_folder()
 
-            self.logger.info(f"===================")
-            self.logger.info(f"leaf_URI: {leaf_URI}")
-            self.logger.info(f"target_folder: {target_folder}")
-            self.logger.info(f"===================")
-
-            if idx > 10:
-                break
-
-            # self.process_leaf_folder(leaf_folder)
 
 if __name__ == "__main__":
     # uri_list = [
@@ -99,5 +162,26 @@ if __name__ == "__main__":
     logger = get_logger("__main__")
     
     data_generator_s3 = DataGeneratorS3()
-    src_uri = "s3://occupancy-dataset/occ-dataset/vineyards/RJM/"
-    data_generator_s3.generate_bev_dataset(src_uri)
+    # src_URI = "s3://occupancy-dataset/occ-dataset/vineyards/RJM/"
+    
+    leaf_URI_src = \
+        "s3://occupancy-dataset/" \
+        "occ-dataset/" \
+        "vineyards/gallo/" \
+        "2024_06_07_utc/svo_files/front_2024-06-04-10-24-57.svo/1398_to_1540/frame-1400/"
+
+    leaf_URI_dest = \
+        "s3://occupancy-dataset/" \
+        "bev-dataset/" \
+        "vineyards/gallo/" \
+        "2024_06_07_utc/svo_files/front_2024-06-04-10-24-57.svo/1398_to_1540/frame-1400/"
+    
+    # data_generator_s3.generate_bev_dataset(src_URI)
+    
+    logger.info(f"=======================")
+    logger.info(f"leaf_URI_src: {leaf_URI_src}")
+    logger.info(f"leaf_URI_dest: {leaf_URI_dest}")
+    logger.info(f"=======================")
+
+    leafFolder = LeafFolder(leaf_URI_src, leaf_URI_dest)
+    leafFolder.process_folder()
